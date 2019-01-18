43cb697b9debf2637eb29e15ec4f1dde28f47d92
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2019-01-18 10:39:07 +0200

    Tyhjä yhteystiedot luokka lisätty

---
+++ src/sql/Yhteystiedot.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sql;
+
+/**
+ *
+ * @author s1800591
+ */
+public class Yhteystiedot {
+    static String getAjuri() {
+    return "com.mysql.jdbc.Driver";
+  } 
+  
+  static String getUrl() {
+    // muuta tähän oman tietokannan osoite
+    return "jdbc:mysql://10.9.0.60/";
+  }
+  // tähän tulee phpMyAdmin käyttäjätunnus
+  static String getKayttajatunnus() {
+    return "";
+  }
+  
+  static String getSalasana() {
+      // muuta tähän phpMyAdmin salasana kun testaat
+    return "";
+  }
+}
