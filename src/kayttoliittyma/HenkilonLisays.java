0abe66a4145110fbd635929fdaa4640b68be8bc7
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2019-01-23 14:08:35 +0200

    Henkilön lisäysikkuna valmis ja toimii yksinään, muttei vielä linkitetty pääikkunaan.

--- src/kayttoliittyma/HenkilonLisays.java
+++ src/kayttoliittyma/HenkilonLisays.java
@@ -7,6 +7,7 @@ package kayttoliittyma;
 
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import javax.swing.*;
 
 /**
@@ -16,52 +17,73 @@ import javax.swing.*;
 public class HenkilonLisays extends JFrame {
 
     //paneelit
-    private JPanel pIkkuna = new JPanel();
+    private JPanel pPohja = new JPanel(new GridLayout(6,1));
     private JPanel pEtunimi = new JPanel(new FlowLayout(FlowLayout.LEFT));
     private JPanel pSukunimi = new JPanel(new FlowLayout(FlowLayout.LEFT));
-    private JPanel pOsoite = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    //private JPanel pOsoite = new JPanel(new FlowLayout(FlowLayout.LEFT));
     private JPanel pSyntymaaika = new JPanel(new FlowLayout(FlowLayout.LEFT));
     private JPanel pHenkilotunnus = new JPanel(new FlowLayout(FlowLayout.LEFT));
-    private JPanel pButtonit = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+    private JPanel pButtonit = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    
     //labelit
     private JLabel lbEtunimi = new JLabel("Etunimi");
     private JLabel lbSukunimi = new JLabel("Sukunimi");
-    private JLabel lbOsoite = new JLabel("Osoite");
+    //private JLabel lbOsoite = new JLabel("Osoite");
     private JLabel lbSyntymaaika = new JLabel("Syntymäaika");
     private JLabel lbHenkilotunnus = new JLabel("Henkilötunnus");
+    
     //text fieldit
-    private JTextField tfEtunimi = new JTextField();
-    private JTextField tfSukunimi = new JTextField();
-    private JTextField tfOsoite = new JTextField();
-    private JTextField tfSyntymaaika = new JTextField();
-    private JTextField tfHenkilotunnus = new JTextField();
+    private JTextField tfEtunimi = new JTextField(15);
+    private JTextField tfSukunimi = new JTextField(15);
+    //private JTextField tfOsoite = new JTextField();
+    private JTextField tfSyntymaaika = new JTextField(15);
+    private JTextField tfHenkilotunnus = new JTextField(15);
+    
     //buttonit
-    private JButton btLisaa = new JButton();
-    private JButton btPeruuta = new JButton();
+    private JButton btLisaa = new JButton("Lisää");
+    private JButton btPeruuta = new JButton("Peruuta");
 
     public HenkilonLisays() {
 
         this.setTitle("Henkilötietojen syöttö");
-        this.setSize(600, 450);
+        this.setSize(400, 250);
         this.setLocation(200, 100);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         asetteleKomponentit();
-        this.setVisible(true);
+        
 
     }
     private void asetteleKomponentit() {
         
-        pIkkuna.add(pEtunimi);
-        pIkkuna.add(pSukunimi);
-        pIkkuna.add(pSukunimi);
-        pIkkuna.add(pOsoite);
-        pIkkuna.add(pSyntymaaika);
-        pIkkuna.add(pHenkilotunnus);
-        pIkkuna.add(pButtonit);
+        pEtunimi.add(lbEtunimi);
+        pEtunimi.add(tfEtunimi);
+        
+        pSukunimi.add(lbSukunimi);
+        pSukunimi.add(tfSukunimi);
+        
+        pSyntymaaika.add(lbSyntymaaika);
+        pSyntymaaika.add(tfSyntymaaika);
+        
+        pHenkilotunnus.add(lbHenkilotunnus);
+        pHenkilotunnus.add(tfHenkilotunnus);
+        
+        pButtonit.add(btLisaa);
+        pButtonit.add(btPeruuta);
+        
+        pPohja.add(pEtunimi);
+        pPohja.add(pSukunimi);
+        pPohja.add(pSukunimi);
+        //pIkkuna.add(pOsoite);
+        pPohja.add(pSyntymaaika);
+        pPohja.add(pHenkilotunnus);
+        pPohja.add(pButtonit);
+        
+        this.add(pPohja);
         
     }
     public static void main(String[] args) {
         
         HenkilonLisays ikkuna = new HenkilonLisays();
+        ikkuna.setVisible(true);
     }
 }
