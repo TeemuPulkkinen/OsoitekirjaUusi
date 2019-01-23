654b6dc071802258064f7fbf3548e7b5f6811ba7
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2019-01-23 10:05:30 +0200

    Päivityksiä.

--- src/kayttoliittyma/HenkilonLisays.java
+++ src/kayttoliittyma/HenkilonLisays.java
@@ -5,15 +5,15 @@
  */
 package kayttoliittyma;
 
+import java.awt.Container;
 import javax.swing.*;
-
 
 /**
  *
  * @author s1800591
  */
 public class HenkilonLisays extends JFrame {
-    
+
     private JPanel lbIkkuna = new JPanel();
     private JLabel lbEtunimi = new JLabel("Etunimi");
     private JLabel lbSukunimi = new JLabel("Sukunimi");
@@ -27,6 +27,16 @@ public class HenkilonLisays extends JFrame {
     private JTextField tfHenkilotunnus = new JTextField();
     private JButton btLisaa = new JButton();
     private JButton btPeruuta = new JButton();
-    
-    
+
+    public HenkilonLisays() {
+        Container paneeli = null;
+        
+        GroupLayout ikkunaAsettelu = new GroupLayout(paneeli);
+        paneeli.setLayout(ikkunaAsettelu);
+        
+        ikkunaAsettelu.setAutoCreateGaps(true);
+        ikkunaAsettelu.setAutoCreateContainerGaps(true);
+        
+        
+    }
 }
