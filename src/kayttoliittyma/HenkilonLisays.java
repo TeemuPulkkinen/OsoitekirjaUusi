31a01db4af1efa3024aa24dff52af6b00a72403b
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2019-01-23 09:43:02 +0200

    Henkilön lisäysikkuna aloitettu

---
+++ src/kayttoliittyma/HenkilonLisays.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kayttoliittyma;
+
+import javax.swing.*;
+
+
+/**
+ *
+ * @author s1800591
+ */
+public class HenkilonLisays extends JFrame {
+    
+    private JPanel lbIkkuna = new JPanel();
+    private JLabel lbEtunimi = new JLabel("Etunimi");
+    private JLabel lbSukunimi = new JLabel("Sukunimi");
+    private JLabel lbOsoite = new JLabel("Osoite");
+    private JLabel lbSyntymaaika = new JLabel("Syntymäaika");
+    private JLabel lbHenkilotunnus = new JLabel("Henkilötunnus");
+    private JTextField tfEtunimi = new JTextField();
+    private JTextField tfSukunimi = new JTextField();
+    private JTextField tfOsoite = new JTextField();
+    private JTextField tfSyntymaaika = new JTextField();
+    private JTextField tfHenkilotunnus = new JTextField();
+    private JButton btLisaa = new JButton();
+    private JButton btPeruuta = new JButton();
+    
+    
+}
