ffe4d0e411e5fb860eba0375925fc3bee08625e8
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2019-01-23 10:44:46 +0200

    Päivityksiä 2 (23.1.2019)

--- src/kayttoliittyma/HenkilonLisays.java
+++ src/kayttoliittyma/HenkilonLisays.java
@@ -6,6 +6,7 @@
 package kayttoliittyma;
 
 import java.awt.Container;
+import java.awt.FlowLayout;
 import javax.swing.*;
 
 /**
@@ -14,29 +15,53 @@ import javax.swing.*;
  */
 public class HenkilonLisays extends JFrame {
 
-    private JPanel lbIkkuna = new JPanel();
+    //paneelit
+    private JPanel pIkkuna = new JPanel();
+    private JPanel pEtunimi = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    private JPanel pSukunimi = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    private JPanel pOsoite = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    private JPanel pSyntymaaika = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    private JPanel pHenkilotunnus = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    private JPanel pButtonit = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+    //labelit
     private JLabel lbEtunimi = new JLabel("Etunimi");
     private JLabel lbSukunimi = new JLabel("Sukunimi");
     private JLabel lbOsoite = new JLabel("Osoite");
     private JLabel lbSyntymaaika = new JLabel("Syntymäaika");
     private JLabel lbHenkilotunnus = new JLabel("Henkilötunnus");
+    //text fieldit
     private JTextField tfEtunimi = new JTextField();
     private JTextField tfSukunimi = new JTextField();
     private JTextField tfOsoite = new JTextField();
     private JTextField tfSyntymaaika = new JTextField();
     private JTextField tfHenkilotunnus = new JTextField();
+    //buttonit
     private JButton btLisaa = new JButton();
     private JButton btPeruuta = new JButton();
 
     public HenkilonLisays() {
-        Container paneeli = null;
+
+        this.setTitle("Henkilötietojen syöttö");
+        this.setSize(600, 450);
+        this.setLocation(200, 100);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        asetteleKomponentit();
+        this.setVisible(true);
+
+    }
+    private void asetteleKomponentit() {
         
-        GroupLayout ikkunaAsettelu = new GroupLayout(paneeli);
-        paneeli.setLayout(ikkunaAsettelu);
+        pIkkuna.add(pEtunimi);
+        pIkkuna.add(pSukunimi);
+        pIkkuna.add(pSukunimi);
+        pIkkuna.add(pOsoite);
+        pIkkuna.add(pSyntymaaika);
+        pIkkuna.add(pHenkilotunnus);
+        pIkkuna.add(pButtonit);
         
-        ikkunaAsettelu.setAutoCreateGaps(true);
-        ikkunaAsettelu.setAutoCreateContainerGaps(true);
+    }
+    public static void main(String[] args) {
         
-        
+        HenkilonLisays ikkuna = new HenkilonLisays();
     }
 }
